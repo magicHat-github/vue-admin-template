e9b4ea38ef875a0323cbe2973460c78de1c1afa1
IncredibleWu <940158806@qq.com>
2019-09-04 00:34:53 +0800

    参数管理的页面逻辑

--- src/views/system/param/index.vue
+++ src/views/system/param/index.vue
@@ -17,7 +17,7 @@
           </el-header>
           <!-- 树 -->
           <el-main>
-            <el-tree :data="treeData" :props="defaultProps" @node-click="handleNodeClick" />
+            <el-tree v-loading="Loading" element-loading-text="Loading" accordion :data="treeData" :props="defaultProps" @node-click="handleNodeClick" />
           </el-main>
         </el-container>
       </el-aside>
@@ -30,22 +30,10 @@
           <el-form :inline="true" :model="searchData" class="demo-form-inline">
             <!-- 组织机构下拉框 -->
             <el-form-item label="参数类型:">
-              <el-select
-                v-model="searchData.paramType"
-                filterable
-                placeholder="请选择"
-                size="mini"
-              >
-                <el-option
-                  v-for="item in paramTypeList"
-                  :key="item.id"
-                  :lable="item.value"
-                  :value="item.value"
-                />
-              </el-select>
+              <el-input v-model="searchData.paramType" placeholder="参数类型" clearable size="mini" @keyup.enter.native="handleFilter" />
             </el-form-item>
-            <el-form-item label="参数项（名称）:">
-              <el-input v-model="searchData.paramName" clearable size="mini" @keyup.enter.native="handleFilter" />
+            <el-form-item label="参数项:">
+              <el-input v-model="searchData.paramName" placeholder="参数项" clearable size="mini" @keyup.enter.native="handleFilter" />
             </el-form-item>
             <el-form-item>
               <el-button size="mini" type="primary" icon="el-icon-search" @click="fetchData">查询</el-button>
@@ -56,7 +44,7 @@
         <!-- 增删改按钮框 -->
         <el-card class="tableData">
           <div>
-            <el-link class="itemAction" type="primary" icon="el-icon-plus" @click="goto">增加</el-link>
+            <el-link class="itemAction" type="primary" icon="el-icon-plus" @click="addParam">增加</el-link>
             <el-link class="itemAction" type="danger" icon="el-icon-delete" @click="deleteCheck">删除</el-link>
             <el-link class="itemAction" type="warning" icon="el-icon-edit" @click="updateCheck">修改</el-link>
             <el-link class="itemAction" type="primary" icon="el-icon-upload2" @click="importParam">导入</el-link>
@@ -67,7 +55,7 @@
 
           <el-table
             ref="multipleTable"
-            v-loading="listLoading"
+            v-loading="Loading"
             element-loading-text="Loading"
             :data="list"
             tooltip-effect="dark"
@@ -83,27 +71,27 @@
             <el-table-column prop="paramName" label="参数项" align="center">
               <template slot-scope="scope">{{ scope.row.paramName }}</template>
             </el-table-column>
-            <el-table-column prop="paramValue" label="参数值" align="center" />
-            <el-table-column prop="status" label="启用标记" sortable="true" align="center">
-              <template slot-scope="scope"><el-tag>{{ scope.row.status }}</el-tag></template>
+            <el-table-column prop="paramValue" label="参数值" align="center">
+              <template slot-scope="scope">{{ scope.row.paramValue }}</template>
+            </el-table-column>
+            <el-table-column class-name="status-col" prop="status" label="启用标记" sortable="true" align="center">
+              <template slot-scope="scope">
+                <el-tag :type="scope.row.status === 1 ? 'success' : 'info'">
+                  {{ scope.row.status === 1 ? "是" : "否" }}
+                </el-tag>
+              </template>
             </el-table-column>
             <el-table-column label="操作" align="center">
-              <el-link class="itemAction" type="primary" icon="el-icon-plus" @click="goto" />
-              <el-link class="itemAction" type="warning" icon="el-icon-edit" @click="update1" />
-              <el-link class="itemAction" type="danger" icon="el-icon-delete" @click="delete1" />
-              <el-link class="itemAction" type="primary" icon="el-icon-upload2" @click="importParam" />
-              <el-link class="itemAction" type="primary" icon="el-icon-download" @click="exportParam" />
-            </el-table-column>
+              <template slot-scope="{row}">
+                <el-link class="itemAction" type="primary" icon="el-icon-plus" @click="addParam" />
+                <el-link class="itemAction" type="warning" icon="el-icon-edit" @click="updateParam(row.id)" />
+                <el-link class="itemAction" type="danger" icon="el-icon-delete" @click="deleteParam(row.id)" />
+              </template>
             </el-table-column>
           </el-table>
           <!-- 分页部分 -->
           <div class="block">
-            <el-pagination
-              :current-page.sync="currentPage1"
-              :page-size="70"
-              layout="prev, pager, next, jumper"
-              :total="1000"
-            />
+            <pagination v-show="dataCount>0" :total="dataCount" :page.sync="page.pageNum" :limit.sync="page.pageSize" @pagination="fetchData" />
           </div>
         </el-card>
       </div>
@@ -113,114 +101,32 @@
 
 <script>
 // import { log } from 'util'
+import Pagination from '@/components/Pagination'
+import { select, searchItem, deleteList } from '@/api/system/param'
 export default {
   name: 'Param',
+  components: { Pagination },
   data() {
     return {
+      //  参数记录列表，用于页面显示区域渲染数据
+      list: null,
+      // 从数据库中查询出来的符合查询条件的参数记录总条目数
+      dataCount: 0,
       /**
-         * 树结构数据
-         */
-      treeData: [
-        {
-          label: '数学题',
-          children: [
-            {
-              label: '几何'
-            }
-          ]
-        },
-        {
-          label: '物理题',
-          children: [
-            {
-              label: '力学'
-            },
-            {
-              label: '电学'
-            }
-          ]
-        }
-      ],
-      /**
-       * 参数类型
+       * 查询字段
        */
-      paramTypes: [
-        '系统',
-        '业务'
-      ],
-      /**
-         * 树结构的默认属性
-         */
-      defaultProps: {
-        children: 'children',
-        label: 'label'
-      },
-
-      /**
-         * 查询字段
-         */
       searchData: {
         paramType: '',
-        paramName: []
+        paramName: ''
       },
-
-      /**
-         * 公司管理
-         */
-      params: [
-        {
-          paramType: '系统参数',
-          paramName: '最大访问数',
-          paramValue: 100000,
-          status: '启用'
-        },
-        {
-          paramType: '系统参数',
-          paramName: '最大访问数',
-          paramValue: 100000,
-          status: '启用'
-        },
-        {
-          paramType: '系统参数',
-          paramName: '最大访问数',
-          paramValue: 100000,
-          status: '启用'
-        },
-        {
-          paramType: '系统参数',
-          paramName: '最大访问数',
-          paramValue: 100000,
-          status: '启用'
-        },
-        {
-          paramType: '系统参数',
-          paramName: '最大访问数',
-          paramValue: 100000,
-          status: '启用'
-        },
-        {
-          paramType: '系统参数',
-          paramName: '最大访问数',
-          paramValue: 100000,
-          status: '启用'
-        },
-        {
-          paramType: '系统参数',
-          paramName: '最大访问数',
-          paramValue: 100000,
-          status: '启用'
-        },
-        {
-          paramType: '系统参数',
-          paramName: '最大访问数',
-          paramValue: 100000,
-          status: '启用'
-        }
-      ],
-
-      /**
-         * 待确认字段
-         */
+      // 分页的页面数据，默认5条一页，默认处于第一页
+      page: {
+        pageSize: 5,
+        pageNum: 1
+      },
+      // load加载动画标志
+      Loading: false,
+      // 表格选择列表
       multipleSelection: [],
       /**
          * 初始显示的页数
@@ -229,20 +135,113 @@ export default {
       currentPage2: 2,
       currentPage3: 3,
       currentPage4: 4,
-      dynamicTags: ['标签一', '标签二', '标签三']
+      dynamicTags: ['标签一', '标签二', '标签三'],
+      /**
+         * 树结构数据
+         */
+      treeData: null,
+      /**
+         * 树结构的默认属性
+         */
+      defaultProps: {
+        children: 'children',
+        label: 'label',
+        id: 'id'
+      }
     }
+  },
+  created() {
+    this.fetchData()
+    this.searchTree()
   },
 
   methods: {
     /**
-       * 树结构的点击事件
-       */
-    handleNodeClick(data) {
-      console.log(data)
+     * 页面刚开始加载数据时，即还未在查询框中输入查询条件时，分页查询参数数据
+     */
+    fetchData() {
+      this.Loading = true
+      const params = {
+        paramType: this.searchData.paramType,
+        paramName: this.searchData.paramType,
+        pageSize: this.page.pageSize,
+        pageNum: this.page.pageNum
+      }
+      select(params).then(result => {
+        const body = result.body
+        // 转换树结构的数据
+        console.log(body.tree)
+        const tree = body.tree.treeNodeList
+        this.treeData = this.transDataToTree(tree)
+        console.log('this is treeData')
+        console.log(this.treeData)
+        // 转换表格数据
+        this.list = body.dataList
+        console.log('this is table data')
+        console.log(this.companys)
+        // 分页信息
+        console.log(body.dataCount)
+        this.total = parseInt(body.dataCount)
+        this.loading = false
+      })
     },
 
     /**
-       * 勾选事件触发的函数
+     * 查询树结构的方法
+     */
+    transDataToTree(arr) {
+      return arr.map(element => {
+        // 这里的element是根节点
+        return this.getChildren(element)
+      })
+    },
+    /**
+     * 查询树结构的方法
+     */
+    getChildren(element) {
+      if (!element.childList) {
+        // 这里的element是叶子节点(叶子节点：即不存在子节点的节点)
+        console.log('this is leafNode')
+        console.log(element)
+        const re = {
+          label: element.name,
+          id: element.id,
+          children: null
+        }
+        return re
+      } else {
+        // 这里的element是非叶子节点(包括根节点)
+        console.log('this is non-leafNode')
+        console.log(element)
+        return {
+          label: element.name,
+          id: element.id,
+          children: this.transDataToTree(element.childList)
+        }
+      }
+    },
+
+    /**
+     * 树的上方区域点击事件
+     */
+    // searchTree() {
+    //   searchTree().then(result => {
+    //     const body = result.body
+    //     this.treeData = body.treeData
+    //     console.log(this.treeData)
+    //   })
+    // },
+    /**
+       * 树结构的点击事件
+       */
+    // handleNodeClick(data) {
+    //   console.log(data)
+    //   this.formInline.name = data.label
+    //   this.fetchByName()
+    // },
+
+    /**
+       * 勾选事件触发的函数,即多选操作
        */
     handleSelectionChange(val) {
       this.multipleSelection = val
@@ -251,30 +250,71 @@ export default {
     /**
        * 跳转到增加界面
        */
-    goto() {
+    addParam() {
       this.$router.push({
         name: 'AddParam'
       })
     },
-    update1() {
-      this.$router.push({
-        name: 'UpdateParam'
+    /**
+     * 跳转至更新页面
+     */
+    updateParam(id) {
+      searchItem(id).then(result => {
+        const body = result.body
+        this.$router.push({
+          name: 'UpdateParam',
+          params: {
+            id: body.id,
+            paramType: body.paramType,
+            paramName: body.paramName,
+            value: body.value,
+            remark: body.remark,
+            status: body.status
+          }
+        })
       })
+    },
+    /**
+     * 对表格多选项进行判定，如果修改的是单条数据则跳转至更新页面
+     */
+    updateCheck() {
+      if (this.multipleSelection.length !== 1) {
+        this.$message({
+          type: 'warning',
+          message: '请选择单个修改选项'
+        })
+      } else {
+        let id = null
+        this.multipleSelection.forEach(item => {
+          id = item.id
+        })
+        this.updateParam(id)
+      }
     },
 
     /**
        * 删除信息
        */
-    delete1() {
+    deleteParam(id) {
       this.$confirm('是否要删除选定信息', '提示', {
         confirmButtonText: '确定',
         cancelButtonText: '取消',
         type: 'warning'
       })
         .then(() => {
-          this.$message({
-            type: 'success',
-            message: '删除成功!'
+          //  确认之后执行删除操作
+          const idList = { idList: [id] }
+          deleteList(idList).then(result => {
+            this.$message({
+              type: 'success',
+              message: '删除成功!'
+            })
+            this.fetchData()
+          }).catch(result => {
+            this.$message({
+              type: 'success',
+              message: '删除失败!'
+            })
           })
         })
         .catch(() => {
@@ -283,6 +323,49 @@ export default {
             message: '已取消删除'
           })
         })
+    },
+    /**
+     * 对表格多选项进行判定，至少选择一行才能执行删除操作
+     */
+    deleteCheck() {
+      if (this.multipleSelection.length === 0) {
+        this.$message({
+          type: 'warning',
+          message: '请选择删除选项'
+        })
+      } else {
+        const idList = { idList: [] }
+        this.multipleSelection.forEach(item => {
+          idList.idList.push(item.id)
+        })
+        this.$confirm('是否要删除选定信息', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        })
+          .then(() => {
+            deleteList(idList).then(result => {
+              console.log(idList)
+              this.$message({
+                type: 'success',
+                message: '删除成功!'
+              })
+              this.fetchData()
+            }).catch(result => {
+              console.log(idList)
+              this.$message({
+                type: 'success',
+                message: '删除失败!'
+              })
+            })
+          })
+          .catch(() => {
+            this.$message({
+              type: 'info',
+              message: '已取消删除'
+            })
+          })
+      }
     }
   }
 }
